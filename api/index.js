1e50b2d551678b11c658704bf0799abb64469dda
nnamdi <adigwennam2@gmail.com>
2023-11-05 22:51:34 +0100

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js"
 import cookieParser from "cookie-parser";
+import path  from 'path'
 
 
 dotenv.config();
@@ -18,6 +19,8 @@ mongoose
     console.log(err);
   });
 
+  const __dirname=path.resolve()
+
 const app = express();
 app.listen(3000, () => {
   console.log("Service running on port 3000");
@@ -28,6 +31,12 @@ app.use(cookieParser())
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
 app.use('/api/listing',listingRouter)
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*',(req,res)=>{
+  res.send(path.join(__dirname,'client','dist','index.html'));
+})
 
 
 //error handling middleware
